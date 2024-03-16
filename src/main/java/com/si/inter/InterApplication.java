3c95957c86633e6f689ac2fa7edd37a0dfd5ecc9
Sunil Agarwal <sunil.agarwal@singularityx.ai>
2024-03-17 02:25:34 +0530

    feat(initial_commit) : [] base springboot application

--- src/main/java/com/si/inter/InterApplication.java
+++ src/main/java/com/si/inter/InterApplication.java
@@ -13,21 +13,6 @@ public class InterApplication {
 
 	@PostConstruct
 	public void start() {
-		FilesystemService  filesystemService =  FilesystemService.getINSTANCE();
-
-		String file1 = new String("file1");
-		String file2 = new String("file2");
-		filesystemService.fcreate(file1);
-		filesystemService.fwrite(file1, "Hello File 1");
-//		System.out.println(filesystemService.fread(file1));
-
-		filesystemService.fcreate(file2);
-		filesystemService.fwrite(file2, "Hello File 2");
-//		System.out.println(filesystemService.fread(file2));
-
-		filesystemService.fwrite(file1, "Hello Again File 1");
-//		System.out.println(filesystemService.fread(file1));
-
 
 	}
 }
